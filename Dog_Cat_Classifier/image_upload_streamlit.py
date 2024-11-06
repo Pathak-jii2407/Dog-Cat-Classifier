f5f80b6df27f78166a72c003194ad1ec99f0b045
ved prakash pathak <156571640+Pathak-jii2407@users.noreply.github.com>
2024-11-06 16:17:20 +0530

    Update image_upload_streamlit.py

--- Dog_Cat_Classifier/image_upload_streamlit.py
+++ Dog_Cat_Classifier/image_upload_streamlit.py
@@ -34,14 +34,14 @@ if uploaded_file is not None:
     predictions = model.predict(img_array)
 
     # Display the image and prediction result
-    fig, ax = plt.subplots(figsize=(1,1))
+    fig, ax = plt.subplots(figsize=(3,3))
     ax.imshow(image)
     
 
     if predictions[0][0] > 0.5:
-        ax.set_title(f'Predicted: Dog, Chances: {predictions[0][0] * 100:.2f}%', fontsize=2)
+        ax.set_title(f'Predicted: Dog, Chances: {predictions[0][0] * 100:.2f}%', fontsize=6)
     else:
-        ax.set_title(f'Predicted: Cat, Chances: {(1 - predictions[0][0]) * 100:.2f}%', fontsize=2)
+        ax.set_title(f'Predicted: Cat, Chances: {(1 - predictions[0][0]) * 100:.2f}%', fontsize=6)
 
     ax.axis('off')
 
